9d331743df39053b23ff1e40780dae31d058c5c5
kstyslo <kacper.styslo@gmail.com>
2021-03-10 14:46:57 +0100

    Changing style of some charts.

--- charts.py
+++ charts.py
@@ -16,7 +16,7 @@ class ChartsMain(BudgetMain):
         self.file_names: list = ['savings.csv', 'expenses.csv']
         self.root = Tk(className="Epic Budget Manager")
         self.root.geometry('1920x1080')
-        self.root.configure(background='white')
+        self.root.configure(background='#808080')
         self.root.resizable(width=False, height=False)
 
     def __call__(self):
@@ -61,59 +61,63 @@ class ChartsMain(BudgetMain):
         while len(last_five_entries) > 5:
             last_five_entries.remove(min(last_five_entries))
         last_five_entries = sorted(last_five_entries, reverse=True)
-        last_five_entries_formatted = [(datetime.fromtimestamp(float(d)).strftime("%Y/%m/%d"), v) for d, v in last_five_entries]
+        last_five_entries_formatted = [(datetime.fromtimestamp(float(d)).strftime("%Y-%m-%d"), v) for d, v in last_five_entries]
         return last_five_entries_formatted
 
     def draw_savings_single_line_chart(self, **kwargs: list):
         savings_dates_for_graph = kwargs['savings_dates_for_graph']
         savings_dates_for_graph_converted = [datetime.fromtimestamp(float(d)).strftime("%y/%m/%d") for d in savings_dates_for_graph]
-        savings_single_line_fig = plt.Figure(figsize=(6, 6.8), dpi=100)
-        savings_single_line_ax = savings_single_line_fig.add_subplot(111)
+        savings_single_line_fig = plt.Figure(figsize=(6, 6.8), dpi=100, facecolor='#808080')
+        savings_single_line_ax = savings_single_line_fig.add_subplot(111, facecolor='#808080')
         savings_single_line = FigureCanvasTkAgg(savings_single_line_fig, self.root)
         savings_single_line.get_tk_widget().place(relx=.15, rely=.32, anchor=CENTER)
         savings_single_line_ax.plot(savings_dates_for_graph_converted, kwargs['savings_values_for_graph'], marker='o', color='green', linewidth=2)
-        savings_single_line_ax.set_title('Savings 2021')
+        savings_single_line_ax.set_title('Savings 2021', fontweight='bold', color='#ffffff')
         savings_single_line_ax.set_ylim(ymin=0)
         savings_single_line_ax.yaxis.grid()
+        savings_single_line_ax.tick_params(colors='#ffffff', which='both')
         savings_single_line_fig.tight_layout()
 
     def draw_pie_chart(self, **kwargs: list):
-        pie_fig = plt.Figure(figsize=(8, 6), dpi=100)
+        pie_fig = plt.Figure(figsize=(8, 6), dpi=100, facecolor='#808080')
         pie_ax = pie_fig.subplots()
         pie_obj = FigureCanvasTkAgg(pie_fig, self.root)
         pie_obj.get_tk_widget().place(relx=.5, rely=.3, anchor=CENTER)
         pie_values = [sum(kwargs['savings_values_for_graph']), sum(kwargs['expenses_values_for_graph']) * (-1)]
-        pie_ax.pie(pie_values, labels=('Savings', 'Expense'), autopct='%1.1f%%', colors=['green', 'red'], startangle=90)
+        explode_value = (0, .1)
+        pie_ax.pie(pie_values, labels=('Savings', 'Expense'), explode=explode_value, shadow=True, autopct='%1.1f%%', colors=['green', 'red'], startangle=90, wedgeprops={"edgecolor": "k", 'linewidth': 2}, textprops={'color': "w"})
         pie_ax.axis('equal')
-        pie_ax.set_title('Budget 2021')
+        pie_ax.set_title('Budget 2021', fontweight='bold', color='#ffffff')
 
     def draw_savings_expenses_bar_chart(self, savings_values_bar_chart, expenses_values_for_bar_chart):
-        bar_savings_expenses_fig = plt.Figure(figsize=(6, 8), dpi=100)
-        bar_savings_expenses_ax = bar_savings_expenses_fig.subplots()
+        bar_savings_expenses_fig = plt.Figure(figsize=(6, 8), dpi=100, facecolor='#808080')
+        bar_savings_expenses_ax = bar_savings_expenses_fig.add_subplot(facecolor='#808080')
         bar_savings_expenses = FigureCanvasTkAgg(bar_savings_expenses_fig, self.root)
         bar_savings_expenses.get_tk_widget().place(relx=.85, rely=.31, anchor=CENTER)
-        bar_savings_expenses_ax.set_title("Savings & Expenses")
+        bar_savings_expenses_ax.set_title("Savings & Expenses", fontweight='bold', color='#ffffff')
         bar_savings_expenses_ax.axhline(y=0, linewidth=2, color='k')
-        bar_savings_expenses_ax.bar('Savings', sum(savings_values_bar_chart), width=0.1, label='Savings', color='green')
-        bar_savings_expenses_ax.bar('Expenses', sum(expenses_values_for_bar_chart), width=0.1, label='Expenses', color='red')
+        bar_savings_expenses_ax.tick_params(colors='#ffffff', which='both')
+        bar_savings_expenses_ax.bar('Savings', savings_values_bar_chart, width=0.5, label='Savings', color='green', edgecolor="black", linewidth=2)
+        bar_savings_expenses_ax.bar('Expenses', [x * (-1) for x in expenses_values_for_bar_chart], width=0.5, label='Expenses', color='red', edgecolor="black", linewidth=2)
 
     def draw_data_table(self, data_for_table: list):
-        data_table_fig = plt.Figure(figsize=(9, 2), dpi=100)
+        data_table_fig = plt.Figure(figsize=(7, 2), dpi=100, facecolor='#808080')
         data_table_ax = data_table_fig.subplots()
         data_table = FigureCanvasTkAgg(data_table_fig, self.root)
         data_table.get_tk_widget().place(relx=.5, rely=.7, anchor=CENTER)
         dt = pd.DataFrame(np.array(data_for_table), columns=list(['Dates', 'Value']))
-        data_table_fig.patch.set_visible(False)
         data_table_ax.axis('off')
         data_table_ax.axis('tight')
-        data_table_ax.set_title("Latest Revenue and Expenses", fontweight="bold", color='red')
-        data_table_ax.table(cellText=dt.values, colLabels=dt.columns, loc='center', colColours=["yellow"] * 2)
+        data_table_ax.set_facecolor('#808080')
+        data_table_ax.set_title("Latest Revenue and Expenses", fontweight="bold", color='#ffffff')
+        # cell_color = ['#808080' for _ in range(len(data_for_table))]
+        data_table_ax.table(cellText=dt.values, colLabels=dt.columns, loc='center', colColours=['yellow'] * 2)  # cellColours=([*cell_color], [*cell_color]))
 
     def show_savings_and_expenses_buttons(self):
         buttons_font_size = font.Font(size=25)
-        savings_button = Button(self.root, command=self.add_income_to_budget, text='+', width=26, bg='#00cc00', fg='#ffffff')
-        expenses_button = Button(self.root, command=self.add_expense_to_budget, text='-', width=26, bg='#FF0000', fg='#ffffff')
-        savings_button.place(x=450, y=950)
+        savings_button = Button(self.root, command=self.add_income_to_budget, text='+', width=26, bg='#00cc00', fg='#ffffff', borderwidth=10)
+        expenses_button = Button(self.root, command=self.add_expense_to_budget, text='-', width=26, bg='#FF0000', fg='#ffffff', borderwidth=10)
+        savings_button.place(x=400, y=950)
         expenses_button.place(x=950, y=950)
         savings_button['font'], expenses_button['font'] = buttons_font_size, buttons_font_size
 
